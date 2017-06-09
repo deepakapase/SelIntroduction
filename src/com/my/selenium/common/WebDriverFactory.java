e33e7cd83d79d747271cef3f6726fd91689937df
Sandeep Kawale <skawaleonline@gmail.com>
2017-06-08 17:41:15 -0700

    New web driver factory

---
+++ src/com/my/selenium/common/WebDriverFactory.java
@@ -0,0 +1,40 @@
+package com.my.selenium.common;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class WebDriverFactory {
+
+	public static WebDriver getDriver(String driverName) throws Exception {
+		WebDriver driver = null;
+		if (driverName == null) {
+			throw new Exception("Driver name missing");
+		}
+
+		switch (driverName) {
+		case "ch":
+			// Chromedriver
+			System.setProperty("webdriver.chrome.driver", "D:\\driversSel\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.get("http://www.google.com");
+			driver.manage().window().maximize();
+			break;
+		case "fd":
+			// Firefox driver
+			System.setProperty("webdriver.gecko.driver", "D:\\driversSel\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			driver.get("http://www.google.com");
+			break;
+		case "ie":
+			// IEdriver
+			System.setProperty("webdriver.ie.driver", "D:\\driversSel\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+			driver.get("http://www.google.com");
+			break;
+		}
+
+		return driver;
+	}
+}
